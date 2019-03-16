7d1d25eba4ff735878268d04beaaa7576588be60
Izuan Ismail <izuan2906@gmail.com>
2019-03-16 14:59:24 +0800

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Izuan Ismail on 16/3/19.
+//  Copyright © 2019 Da'ieMind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
