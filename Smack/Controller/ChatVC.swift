63ca15a8929ba04da3794265aacd29c54d2783d1
Izuan Ismail <izuan2906@gmail.com>
2019-03-16 17:28:46 +0800

    End of Lesson 2

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,27 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Izuan Ismail on 16/3/19.
+//  Copyright © 2019 Da'ieMind. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+    self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+
+    }
+
+}
--- Supporting Files/Smack-Bridging-Header.h
+++ Supporting Files/Smack-Bridging-Header.h
@@ -1,5 +1,9 @@
 //
-//  Use this file to import your target's public headers that you would like to expose to Swift.
+//  Smack-Bridging-Header.h
+//  Smack
+//
+//  Created by Izuan Ismail on 16/3/19.
+//  Copyright © 2019 Da'ieMind. All rights reserved.
 //
 
 #import "SWRevealViewController.h"
