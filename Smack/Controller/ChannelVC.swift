4b80c60e58fa388a3f825d07abdeae46ac166066
Izuan Ismail <izuan2906@gmail.com>
2019-03-29 19:04:26 +0800

    lesson 4

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ChannelVC: UIViewController {
+    
+    // OUTLETS:
+    @IBOutlet weak var loginBtn: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +20,10 @@ class ChannelVC: UIViewController {
         self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
 
     }
+    
+    @IBAction func loginBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    }
+    
+    
 }
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Izuan Ismail on 17/3/19.
+//  Copyright © 2019 Da'ieMind. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
