718a04325f287c2705487f212027dd01d686b0d5
Izuan Ismail <izuan2906@gmail.com>
2019-04-01 18:56:46 +0800

    Lesson-12

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -16,6 +16,9 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var passTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
     
+    // Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +27,7 @@ class CreateAccountVC: UIViewController {
     }
    
     @IBAction func createAcctPressed(_ sender: Any) {
+        guard let name = usernameTxt.text, usernameTxt.text != "" else { return }
         guard let email = emailTxt.text , emailTxt.text != "" else { return }
         guard let pass = passTxt.text, passTxt.text != "" else { return }
         
@@ -32,7 +36,13 @@ class CreateAccountVC: UIViewController {
                 //print("registered user!")
                 AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
                     if success {
-                        print("Logged in user!", AuthService.instance.authToken)
+                        //print("Logged in user!", AuthService.instance.authToken)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print(UserDataService.instance.name, UserDataService.instance.avatarName, UserDataService.instance.avatarColor, UserDataService.instance.email, UserDataService.instance.id)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                     }
                 })
             }
